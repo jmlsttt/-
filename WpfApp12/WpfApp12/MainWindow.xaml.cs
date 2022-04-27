1d0da63dcb160d0540cb37aa21e9bd98a5ee4d24
jmlsttt <91660260+jmlsttt@users.noreply.github.com>
2022-04-28 01:18:03 +0300

    Add files via upload

---
+++ WpfApp12/WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SDKSample
+{
+
+    public partial class MediaTimelineExample : Page
+    {
+        // When the media opens, initialize the "Seek To" slider maximum value
+        // to the total number of miliseconds in the length of the media clip.
+        private void Element_MediaOpened(object sender, EventArgs e)
+        {
+            timelineSlider.Maximum = myMediaElement.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        private void MediaTimeChanged(object sender, EventArgs e)
+        {
+            timelineSlider.Value = myMediaElement.Position.TotalMilliseconds;
+        }
+    }
+}
